33a88f0a182b24cd769d0597d4f4ef6754169199
degracim0020-cti-110 <degracim0020@student.faytechcc.edu>
2026-09-01 14:46:34 -0400

    Add files via upload

---
+++ p1lab1_De Gracia.py
@@ -0,0 +1,21 @@
+# CTI 110
+# P1LAB1
+# De Gracia, M
+# 9/1/26
+# Our first Python program
+
+# Firstly we will great the User
+print("Hello and Good afternoon!")
+
+# Next, ask for first Name
+#print("What's your name?")
+first_name = input("What's your name?")
+
+# Then say back the name 
+print("Nice to meet you", first_name)
+
+# TODO: do the same thing for last_name
+#print("can I get your last name? ")
+last_name = input("can I get your last name? ")
+
+print("Nice to have you here", first_name, last_name)
