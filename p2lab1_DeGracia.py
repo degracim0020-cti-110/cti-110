8e3591d5cda90498ea52dbf5bb771571af7a607f
degracim0020-cti-110 <degracim0020@student.faytechcc.edu>
2026-09-15 17:54:00 +0000

    P2LAB1 Done

---
+++ p2lab1_DeGracia.py
@@ -0,0 +1,22 @@
+"""
+(block comments -- continue until the triple quotes end)
+CTI 110
+P2LAB1
+Michaelangelo De Gracia
+9/15/26
+Get radius,
+"""
+
+PI = 3.14159
+# Input -- get radius
+radius = float(input("what is the radius of the circle?"))
+
+# Calculation -- find diameter, circumference, and area
+# diameter = 2*r, circumference = 2*pi*r, area = pi*r*r
+diameter = 2 * radius
+circumference = 2 * PI * radius
+area = PI * radius * radius
+# Output -- .1f, .2f, 3.f
+print(f"The diameter is {diameter:.1f}.")
+print(f"The circumference is {circumference:.2f}")
+print(f"The area is{area: .3f}")
