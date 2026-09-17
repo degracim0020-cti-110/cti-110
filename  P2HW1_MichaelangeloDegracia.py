ee65b3868f77e3d1d9758fb4bff4ea1d5570884c
degracim0020-cti-110 <degracim0020@student.faytechcc.edu>
2026-09-17 17:23:14 +0000

    renamed

---  P2HW1_MichaelangeloDegracia.py
+++  P2HW1_MichaelangeloDegracia.py
@@ -23,10 +23,10 @@ print() # That gives 2 newlines, so would print("\n")
 
 print("----Travel Expenses Results----")
 print("\n") # 2 new lines
-print("Location:", Travel_location)
-print("Budget:", start)
-print("Gas:", sub_this1)
-print("Accomodatons:", sub_this2)
-print("Food:", sub_this3)
+print("Location: ", Travel_location)
+print("Budget: $", start)
+print("Gas: $", sub_this1)
+print("Accomodatons: $", sub_this2)
+print("Food: $", sub_this3)
 
-print("Leftover Balance:", answer)
+print("Leftover Balance: $", answer)
