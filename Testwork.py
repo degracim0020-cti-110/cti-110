c0a586b03654883c02e43ae6fb2b9010a2f93e1c
degracim0020-cti-110 <degracim0020@student.faytechcc.edu>
2026-09-17 18:09:07 +0000

    Test work

---
+++ Testwork.py
@@ -0,0 +1,8 @@
+print("Brandon and Tycen should be blacklisted from the internet")
+
+print(f"{'They\'re the most evil individuals to step foot on this planet':<13}")
+answer = input("Do you agree? (yes/no): ")
+if answer == "yes":
+    print("You agree with me good, you are a good person and will be rewarded with a free trip to the Bahamas")
+else:
+    print("You disagree with me? you are a fool, you will be blacklisted from the internet as well")
